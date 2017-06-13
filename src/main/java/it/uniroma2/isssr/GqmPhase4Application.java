58e3db871f3b371d218b60c23917461ac66076a5
Ovidiu Daniel Barba <ovi.daniel.b@gmail.com>
2017-06-13 10:05:49 +0200

    First commit

---
+++ src/main/java/it/uniroma2/isssr/GqmPhase4Application.java
@@ -0,0 +1,12 @@
+package it.uniroma2.isssr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqmPhase4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GqmPhase4Application.class, args);
+	}
+}
---
+++ src/main/java/it/uniroma2/isssr/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.uniroma2.isssr;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GqmPhase4Application.class);
+	}
+
+}
