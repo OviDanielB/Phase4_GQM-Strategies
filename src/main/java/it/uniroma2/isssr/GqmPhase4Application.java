f8aa27ef12c9601f5956921c795533290def0cc5
Ovidiu Daniel Barba <ovi.daniel.b@gmail.com>
2017-07-13 15:20:22 +0200

    pom modified for war packaging

--- src/main/java/it/uniroma2/isssr/GqmPhase4Application.java
+++ src/main/java/it/uniroma2/isssr/GqmPhase4Application.java
@@ -2,9 +2,16 @@ package it.uniroma2.isssr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GqmPhase4Application {
+public class GqmPhase4Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GqmPhase4Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GqmPhase4Application.class, args);
